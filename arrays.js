914ea026d1754ebae25e47d53b77002d291cb37e
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-18 19:25:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element)
-array=[]
 {
-array.push(element);
-}
 
 function destructivelyAddElementToBeginningOfArray (array, element)
 array = []
