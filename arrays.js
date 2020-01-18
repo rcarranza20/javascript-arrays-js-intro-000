046cd696c62aad8fa2974829830c1f4b5ad4c22b
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-18 19:50:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
-  array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
